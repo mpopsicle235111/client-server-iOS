d6b8020709602c0ee6ab0e96c48c55ebf7d12401
Anton  Lebedev <lebedev_anton@mail.ru>
2021-12-08 01:56:15 +0300

    Initiate project

---
+++ Hometask 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hometask 1.0
+//
+//  Created by Anton Lebedev on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
